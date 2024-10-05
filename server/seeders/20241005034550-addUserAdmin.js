e107c2d4cec2556e62be34a298c441190253060d
Adityasputra <aditsputra.cv@gmail.com>
2024-10-05 10:52:04 +0700

    Feat: create a Seeder for user admin

---
+++ server/seeders/20241005034550-addUserAdmin.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const { hashPassword } = require("../helpers/bcryptjsHelper");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          username: "Admin",
+          email: "admin@gmail.com",
+          password: hashPassword("admin123"),
+          role: "admin",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
