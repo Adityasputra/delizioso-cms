94af317eed12bcd1a69afbf025bbd1f3bf9cd949
Adityasputra <aditsputra.cv@gmail.com>
2025-02-02 03:33:14 +0700

    fix : change inlay routes

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -7,49 +7,50 @@ import AddStaffPage from "./pages/Form/AddStaffPage";
 import CategoryPage from "./pages/Category/CategoryPage";
 import CuisineDetailPage from "./pages/Cuisine/CuisineDetailPage";
 import EditCuisinePage from "./pages/Form/EditCuisinePage";
+import MobileWarning from "./pages/MobileWarning";
+
+const checkAuth = () => {
+  const token = localStorage.getItem("access_token");
+  return token ? null : redirect("/login");
+};
+
+const checkAlreadyLoggedIn = () => {
+  const token = localStorage.getItem("access_token");
+  return token ? redirect("/") : null;
+};
+
+const checkMobile = () => {
+  if (window.innerWidth < 768) {
+    return redirect("/mobile-warning");
+  }
+  return null;
+};
 
 const router = createBrowserRouter([
   {
     path: "/login",
     element: <LoginPage />,
-    loader: () => {
-      if (localStorage.access_token) return redirect("/");
-      return null;
-    },
+    loader: checkAlreadyLoggedIn,
   },
   {
     path: "/",
     element: <MainLayout />,
-    loader: () => {
-      if (!localStorage.access_token) return redirect("/login");
-      return null;
+    loader: async () => {
+      await checkAuth();
+      return checkMobile();
     },
     children: [
-      {
-        path: "",
-        element: <Dashboard />,
-      },
-      {
-        path: "/add/cuisine",
-        element: <AddCuisinePage />,
-      },
-      {
-        path: "/add/staff",
-        element: <AddStaffPage />,
-      },
-      {
-        path: "/categories",
-        element: <CategoryPage />,
-      },
-      {
-        path: "/cuisine/:id/edit",
-        element: <EditCuisinePage />,
-      },
-      {
-        path: "/cuisine/:id/detail",
-        element: <CuisineDetailPage />,
-      },
+      { path: "", element: <Dashboard /> },
+      { path: "add/cuisine", element: <AddCuisinePage /> },
+      { path: "add/staff", element: <AddStaffPage /> },
+      { path: "categories", element: <CategoryPage /> },
+      { path: "cuisine/:id/edit", element: <EditCuisinePage /> },
+      { path: "cuisine/:id/detail", element: <CuisineDetailPage /> },
     ],
+  },
+  {
+    path: "/mobile-warning",
+    element: <MobileWarning />,
   },
 ]);
 
