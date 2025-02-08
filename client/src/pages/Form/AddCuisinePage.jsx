0a69beb6fd4b6d0705aa0386678278d9a5cfd1b4
Adityasputra <aditsputra.cv@gmail.com>
2025-02-09 00:14:56 +0700

    style : improved appearance and fixed the bug of photos not uploading

--- client/src/pages/Form/AddCuisinePage.jsx
+++ client/src/pages/Form/AddCuisinePage.jsx
@@ -1,112 +1,110 @@
 import axios from "../../services/axiosServices";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { Bounce, toast } from "react-toastify";
+import Button from "../../components/ui/Button";
 
 export default function AddCuisinePage() {
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [description, setDescription] = useState("");
-  const [category, setCategory] = useState("");
-  const [file, setFile] = useState(null);
-  const [categories, setCategories] = useState([]);
+  const [form, setForm] = useState({
+    name: "",
+    price: "",
+    description: "",
+    category: "",
+    file: null,
+  });
 
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
   const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
+    const file = e.target.files[0];
+    setForm((prev) => ({ ...prev, file }));
   };
 
   const handleAddCuisine = async (e) => {
     e.preventDefault();
+    setLoading(true);
 
     try {
+      const { name, price, description, category, file } = form;
+      if (!name || !price || !description || !category) {
+        return toast.error("All fields are required!");
+      }
+
       const dataUploadImage = new FormData();
       dataUploadImage.append("name", name);
       dataUploadImage.append("price", price);
       dataUploadImage.append("description", description);
       dataUploadImage.append("CategoryId", category);
+      if (file) dataUploadImage.append("imgUrl", file);
       if (file) {
-        dataUploadImage.append("imgUrl", file);
+        console.log("File selected:", file);
       }
 
-      await axios({
-        method: "POST",
-        url: "/cuisines",
+      console.log("Data before sending:", {
+        name,
+        price,
+        description,
+        category,
+        file,
+      });
+
+      const token = localStorage.getItem("access_token");
+      console.log("Token:", token);
+      if (!token) {
+        console.error("No access token found!");
+      }
+
+      const response = await axios.post("/cuisines", dataUploadImage, {
         headers: {
-          Authorization: `Bearer ${localStorage.access_token}`,
+          Authorization: `Bearer ${token}`,
           "Content-Type": "multipart/form-data",
         },
-        data: dataUploadImage,
       });
 
-      toast.success("Successfully added cuisine!", {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-        transition: Bounce,
-      });
+      console.log("Response from server:", response.data);
 
+      toast.success("Successfully added cuisine!", { transition: Bounce });
       navigate("/");
     } catch (error) {
-      if (error.response) {
-        const errorData = error.response.data.message;
-        errorData.map((el) =>
-          toast.error(`${el}`, {
-            position: "top-right",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-          })
-        );
-      } else {
-        toast.error("Network error, please try again later!", {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-      }
-
-      console.error(error);
+      console.error("Error adding cuisine:", error);
+      console.log("Full error response:", error.response);
+      toast.error(error.response?.data?.message || "Failed to add cuisine.");
+    } finally {
+      setLoading(false);
     }
   };
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const { data } = await axios({
-          method: "GET",
-          url: "/categories",
-          headers: {
-            Authorization: `Bearer ${localStorage.access_token}`,
-          },
-        });
-
-        setCategories(data);
-      } catch (error) {
-        console.log(error);
+  const fetchCategories = useCallback(async () => {
+    try {
+      const token = localStorage.getItem("access_token");
+      if (!token) {
+        console.error("No access token found!");
       }
-    };
 
-    fetchCategories();
+      const { data } = await axios.get("/categories", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setCategories(data);
+    } catch (error) {
+      console.log("Error fetching categories:", error);
+    }
   }, []);
 
+  useEffect(() => {
+    fetchCategories();
+  }, [fetchCategories]);
+
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+    <div className="flex items-center justify-center min-h-screen">
       <section className="max-w-4xl w-full p-8 mx-auto bg-white rounded-lg shadow-md">
-        <h2 className="text-3xl font-semibold text-center text-[#B22222] mb-8 capitalize">
+        <h2 className="text-3xl font-semibold text-center text-[#181818] mb-8 capitalize">
           Add Cuisine
         </h2>
         <form onSubmit={handleAddCuisine}>
@@ -118,10 +116,11 @@ export default function AddCuisinePage() {
               <input
                 id="name"
                 type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                name="name"
+                value={form.name}
+                onChange={handleChange}
                 placeholder="Enter cuisine name"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none hover:border-blue-400 transition-colors"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-[#181818] focus:ring focus:ring-[#181818]/50 focus:ring-opacity-40 focus:outline-none hover:border-[#181818] transition-all"
               />
             </div>
 
@@ -132,33 +131,25 @@ export default function AddCuisinePage() {
               <input
                 id="price"
                 type="number"
-                value={price}
-                onChange={(e) => setPrice(e.target.value)}
+                name="price"
+                value={form.price}
+                onChange={handleChange}
                 min="0"
                 placeholder="Enter price"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none hover:border-blue-400 transition-colors"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-[#181818] focus:ring focus:ring-[#181818]/50 focus:ring-opacity-40 focus:outline-none hover:border-[#181818] transition-all"
               />
             </div>
 
             <div>
-              <label
-                className="text-gray-700 font-medium"
-                htmlFor="imageUpload"
-              >
+              <label className="text-gray-700 font-medium" htmlFor="file">
                 Upload Image
               </label>
               <input
-                id="imageUpload"
+                id="file"
                 type="file"
-                name="file"
                 onChange={handleFileChange}
                 accept="image/*"
-                className="block w-full text-sm text-gray-500
-                file:mr-4 file:py-2 file:px-4
-                file:rounded-md file:border-0
-                file:text-sm file:font-semibold
-                file:bg-blue-50 file:text-[#8B4513]
-                hover:file:bg-blue-100 transition-colors"
+                className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-[#181818] file:text-[#fffcf9] hover:file:bg-[#383838] transition-all cursor-pointer"
               />
             </div>
 
@@ -171,12 +162,13 @@ export default function AddCuisinePage() {
               </label>
               <textarea
                 id="description"
+                name="description"
                 rows="4"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
+                value={form.description}
+                onChange={handleChange}
                 placeholder="Enter description"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none hover:border-blue-400 transition-colors"
-              ></textarea>
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-[#181818] focus:ring focus:ring-[#181818]/50 focus:ring-opacity-40 focus:outline-none hover:border-[#181818] transition-all"
+              />
             </div>
 
             <div className="md:col-span-2">
@@ -184,10 +176,11 @@ export default function AddCuisinePage() {
                 Categories
               </label>
               <select
-                id="CategoryId"
-                value={category}
-                onChange={(e) => setCategory(e.target.value)}
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none hover:border-blue-400 transition-colors"
+                id="category"
+                name="category"
+                value={form.category}
+                onChange={handleChange}
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-[#181818] focus:ring focus:ring-[#181818]/50 focus:ring-opacity-40 focus:outline-none hover:border-[#181818] transition-all"
               >
                 <option value="">Select a category</option>
                 {categories.map((categoryItem) => (
@@ -200,9 +193,9 @@ export default function AddCuisinePage() {
           </div>
 
           <div className="flex justify-end mt-8">
-            <button className="px-6 py-3 text-white bg-[#8B4513] rounded-md hover:bg-[#B22222] focus:outline-none transition-colors">
-              Save
-            </button>
+            <Button type="submit" disabled={loading}>
+              {loading ? "Saving..." : "Save"}
+            </Button>{" "}
           </div>
         </form>
       </section>
