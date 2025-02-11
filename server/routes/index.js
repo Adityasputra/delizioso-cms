b92bdb17e9c3fb71f62e141d8efa8b178d9a49b6
Adityasputra <aditsputra.cv@gmail.com>
2025-02-12 06:57:46 +0700

    feat: added testing for connect redis

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,6 +10,18 @@ const routerProfile = require("./profile");
 const { authentication } = require("../middlewares/auth");
 const errorHandle = require("../middlewares/errorHandle");
 
+const redis = require("../config/redis");
+
+router.get("/test-redis", async (req, res) => {
+  try {
+    await redis.set("test-message", "Redis is working!");
+    const message = await redis.get("test-message");
+    res.status(200).json({ success: true, message });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
 router.get("/", (req, res) => {
   res.status(200).json({ message: "Server is running..." });
 });
