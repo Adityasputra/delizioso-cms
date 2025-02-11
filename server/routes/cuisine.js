5c05c2ad76323ba40f84a056e21d607588e09704
Adityasputra <aditsputra.cv@gmail.com>
2025-02-12 06:58:49 +0700

    feat: added middleware to routes cuisine

--- server/routes/cuisine.js
+++ server/routes/cuisine.js
@@ -1,18 +1,18 @@
-const router = require("express").Router();
-const upload = require("../middlewares/multer");
+const express = require("express");
+const cacheMiddleware = require("../middlewares/redis");
 const {
-  addCuisine,
   getAllCuisine,
   getDetailCuisine,
-  editCuisine,
+  addCuisine,
   removeCuisine,
 } = require("../controllers/cuisineController");
-const { authorization } = require("../middlewares/auth");
 
-router.post("/", upload.single("imgUrl"), addCuisine);
-router.get("/", getAllCuisine);
-router.get("/:id/detail", getDetailCuisine);
-router.put("/:id/edit", authorization, upload.single("imgUrl"), editCuisine);
-router.delete("/:id/remove", authorization, removeCuisine);
+const router = express.Router();
+
+router.get("/", cacheMiddleware("cuisine-list", 300), getAllCuisine);
+router.get("/:id", cacheMiddleware("cuisine-detail", 300), getDetailCuisine);
+
+router.post("/", addCuisine);
+router.delete("/:id", removeCuisine);
 
 module.exports = router;
