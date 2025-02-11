1eac6e2330cf1ab66b9abf36a83b734576916e9e
Adityasputra <aditsputra.cv@gmail.com>
2025-02-11 22:50:32 +0700

    feat: added shutdown function and handle error on server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,21 @@
 const app = require("./src/app");
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on PORT : ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
+server.on("error", (err) => {
+  console.error("❌ [SERVER ERROR]:", err);
+});
+
+const shutdown = () => {
+  console.log("\n🔴 Shutting down server...");
+  server.close(() => {
+    console.log("✅ Server closed. Cleanup complete.");
+    process.exit(0);
+  });
+};
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
